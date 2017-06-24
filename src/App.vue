de8ed5cd6a02b00a22efc7353560cde1e2ef0f3d
CharlesYwj <ywj0627@163.com>
2017-06-24 08:15:26 +0800

    Blog Manager

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app" class="container">
+    <navbar></navbar>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import Navbar from "./components/Navbar";
+export default {
+  name: 'app',
+  components:{
+    Navbar
+  }
+}
+</script>
+
+<style lang="scss">
+@import "../static/sass/blog";
+
+.container{
+  display: flex;
+}
+</style>
---
+++ src/components/Topnav.vue
@@ -0,0 +1,20 @@
+<template>
+    <header>
+        <div>Blog manager</div>
+        <ul>
+        </ul>
+        <div class="pull-right">
+            <span>管理员</span>
+        </div>
+    </header>
+</template>
+<<script>
+export default {
+  
+}
+</script>
+<style lang="scss">
+    header{
+        background: #345edf;
+    }
+</style>
